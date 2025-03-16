5a53bf247951807446354cfd572274e214b2efb5
Ammar Obad Al Wasim <ammar211080@gmail.com>
2025-03-16 06:01:49 +0400

    fix files path #1

--- frontend/src/components/QrCode.jsx
+++ frontend/src/components/QrCode.jsx
@@ -1,9 +1,9 @@
-import QRCode from "../assets/images/QrCode.png"
+import QRCodeSrc from "../assets/images/QrCode.png"
 
 const QrCode = () => {
     return (
         <div className="flex flex-col lg:flex-row gap-2 lg:gap-4 w-4/12">
-            <img src={QRCode} alt={QRCode} className="lg:min-w-full lg:min-h-fit" />
+            <img src={QRCodeSrc} alt={QRCodeSrc} className="lg:min-w-full lg:min-h-fit" />
             <p className="text-sm px-2 py-1.5 lg:p-0 border-[0.5px] rounded-lg lg:border-0 lg:rounded-none lg:text-xl font-extralight leading-4 lg:leading-5 text-justify min-w-full lg:min-w-fit ">Or visit us from here</p>
         </div>
     )
