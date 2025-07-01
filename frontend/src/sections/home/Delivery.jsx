c55ad2d758a11d832a44f845125128732ef25160
amxr21 <ammar211080@gmail.com>
2025-07-01 13:39:05 +0400

    update style

--- frontend/src/sections/home/Delivery.jsx
+++ frontend/src/sections/home/Delivery.jsx
@@ -13,7 +13,7 @@ const Delivery = () => {
                     <img src={DeliveryBannerMobile} className="w-full" alt="" />
                     
                 }
-                <div className="banner-text font-[Stinger] text-5xl lg:text-7xl font-bold text-white absolute w-full h-full px-8 lg:px-16 top-0 uppercase flex items-center justify-center transform lg:-translate-x-1/2 lg:-translate-y-1/2">
+                <div className="banner-text font-[Stinger] text-5xl lg:text-7xl font-bold text-white absolute w-full h-full px-8 lg:px-16 top-0 uppercase flex items-center justify-center transform   lg:-translate-y-2">
                     <p className="text-justify">No matter where you are in Detroit, Michigan, <span className="text-amber-400">Blazin' Grill</span> is ready to bring the flavor straight to you!</p>
                 </div>
             </div>
